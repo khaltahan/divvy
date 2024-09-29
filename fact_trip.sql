9472cad354d692a496289ad99f6e13298dfec1f1
Khalid Altahan <88383855+khaltahan@users.noreply.github.com>
2024-09-29 15:13:57 -0400

    Update and rename fact_ride_time.sql to fact_trip.sql

---
+++ fact_trip.sql
@@ -0,0 +1,16 @@
+CREATE EXTERNAL TABLE [dbo].[fact_trip] WITH(
+    LOCATION = 'star/fact_trip',
+    DATA_SOURCE = [azure_dfs_core_windows_net],
+    FILE_FORMAT = [SynapseDelimitedTextFormat]
+) AS (
+    SELECT 
+        CAST(t.trip_id AS bigint) AS trip_id,
+        DATEDIFF(MINUTE, t.start_at, t.ended_at) AS trip_duration,
+        t.rider_id,
+        t.start_station_id AS station_id,
+        YEAR(d.date) - YEAR(r.birthday) AS rider_age,
+        d.date_id AS trip_date
+    FROM staging_trip t
+    JOIN dim_rider r ON t.rider_id = r.rider_id
+    JOIN dim_date d ON CAST(t.start_at AS DATE) = d.date
+);
